afeb4c21610ffaf8dbd27dcd52111d311b5c0a9c
Matija Savic <makisha@elfak.rs>
2023-04-28 13:47:44 +0200

    Proba

--- SistemskoProgramiranje1/SistemskoProgramiranje1/Program.cs
+++ SistemskoProgramiranje1/SistemskoProgramiranje1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace Projekat;
+
+
+public static class HttpServer
+{
+    static async Task Main(String[] args)
+    {
+        var listener = new HttpListener();
+        //listener.Prefixes.Add("https://127.0.0.1:8080/");
+        listener.Prefixes.Add("http://127.0.0.1:8080/");
+        listener.Start();
+
+        while (listener.IsListening)
+        {
+            var context = listener.GetContext();
+
+            new Thread(() =>
+            {
+                
+                Console.WriteLine("OK");
+            }).Start();
+            
+        }
+        
+    }
+}
